923bbec276419b6bd21f3f318ddaf89d098eeba8
Taylor Facen <TaylorFay@Taylors-MacBook-Air-2.local>
2016-10-01 10:16:36 -0400

    Finished

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -17,10 +17,60 @@ class ViewController: UIViewController {
     
     
     
+func averageIsAbove(a: Double, b: Double, c: Double) -> Bool {
+    switch Int((a + b + c) / 3) {
+    case 75...99999:
+        return true
+    default:
+        return false
+    }
+    }
     
+    func passwordCombo(username: String, password: Int) -> Bool {
+        let usernameTest: Bool
+        let passwordTest: Bool
+        
+        switch username {
+        case "Jerry", "Elaine", "Michael":
+            usernameTest = true
+        default:
+            usernameTest = false
+        }
+        
+        if password % 3 == 0 {
+            passwordTest = true
+        }
+        else {
+            passwordTest = false
+        }
+        
+        if usernameTest && passwordTest {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
+
+  
     // Implement your functions here!
-    
-    
     
     
 }
