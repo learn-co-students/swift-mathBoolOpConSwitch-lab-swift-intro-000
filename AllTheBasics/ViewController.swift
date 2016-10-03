c5664cba163e2b48a27393fc7a56dda444a1f024
John Richardson <richardsonjm@gmail.com>
2016-10-03 11:54:25 -0400

    More IFTTT

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -20,7 +20,41 @@ class ViewController: UIViewController {
     
     // Implement your functions here!
     
+    func averageIsAbove(_ arg1:Double, _ arg2:Double, _ arg3:Double) -> Bool {
+        let quotient = (arg1 + arg2 + arg3) / 3.0
+
+        if quotient > 75.0 {
+            return true
+        } else {
+            return false
+        }
+    }
     
+    func passwordCombo(username: String, password: Int) -> String {
+        let goodUsername = ["Jerry", "Elaine", "Michael"].contains(username)
+        let goodPassword = password % 3 == 0
     
+        if goodUsername && goodPassword {
+            return "Welcome!"
+        } else {
+            return "Access Denied"
+        }
+    }
     
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
 }
