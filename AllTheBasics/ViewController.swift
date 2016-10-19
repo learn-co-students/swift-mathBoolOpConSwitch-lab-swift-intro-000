1c97cbd9881ccb2ef281bbd8d2f24430a84afb50
Dor Rondel <dor.rondel@gmail.com>
2016-10-19 07:32:41 -0400

    added lab

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,7 +15,34 @@ class ViewController: UIViewController {
         
     }
     
-    // Implement your functions here!
+    func averageIsAbove75(_ a: Double, _ b: Double, _ c: Double) -> Bool {
+        if (a+b+c) / 3 > 75 {
+            return true
+        }
+        return false
+    }
     
+    func passwordCombo(username: String, password: Int) -> String {
+        if (username == "Jerry" || username == "Elaine" || username == "Michael") && password % 3 == 0 {
+            return "Welcome!"
+        }
+        return "Access Denied"
+    }
 
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "💋":
+                return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
 }
