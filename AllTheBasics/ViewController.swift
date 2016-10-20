7c3bcdcb97336bc93dfd9b2b3c7767036a81a5a1
fete <d1swaay@gmail.com>
2016-10-19 20:02:13 -0400

    Creates a couple functions to show if and switch statement use.

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -16,6 +16,38 @@ class ViewController: UIViewController {
     }
     
     // Implement your functions here!
+    func averageIsAbove75(_ num1: Double, _ num2: Double, _ num3: Double) -> Bool {
+        var avg = (num1 + num2 + num3) / 3
+        switch avg > 75 {
+        case true:
+            return true
+        default:
+            return false
+        }
+        
+    }
+    
+    func passwordCombo(_ username: String, password: Int) -> String {
+        var usernameCheck = false
+        
+        switch username {
+        case "Jerry":
+            usernameCheck = true
+        case "Elaine":
+            usernameCheck = true
+        case "Michael":
+            usernameCheck = true
+        default:
+            return "Access Denied"
+        }
+        
+        if password / 3 == 0 {
+                return "Welcome!"
+        } else {
+            return "Access Denied"}
+        
+    }
+    
     
 
 }
