14c34369b351174c4e9c3b28a6bdc28e11d84d6d
chandu <chandu1729@gmail.com>
2016-10-25 16:54:02 -0400

    Revision Required

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -12,10 +12,30 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//       let result = self.averageIsAbove75(_number1: 55,_number2: 75,_number3: 100)
+//        print(result)
+//        
     }
     
     // Implement your functions here!
-    
+    func averageIsAbove75(_number1: Double,_number2: Double,_number3: Double) -> Bool {
+        let sum = _number1+_number2+_number3
+        let average = sum/3
+        
+        switch average>75 {
+        case true:
+            return true
+        default:
+            return false
+        }
+    }
+    func passwordCombo(username: String,password: Int) -> String {
+        switch (username,password) {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
+        }
+    }
 
 }
