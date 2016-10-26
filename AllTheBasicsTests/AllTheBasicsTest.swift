fc10ab228ec7c6222bb6559feaee945f8db46c06
pacosta613 <pedro.acosta613@gmail.com>
2016-10-26 17:48:14 -0400

    Fixed errors from Tests

--- AllTheBasicsTests/AllTheBasicsTest.swift
+++ AllTheBasicsTests/AllTheBasicsTest.swift
@@ -23,13 +23,13 @@ class AllTheBasicsTest: XCTestCase {
     
     func testAverageIsAbove75() {
 
-        XCTAssertFalse(testVC.averageIsAbove(76, 75, 71.9), "Average is below 75, it should return false."  )
+        XCTAssertFalse(testVC.averageIsAbove75(num1: 76, num2: 75, num3: 71.9), "Average is below 75, it should return false."  )
         
-        XCTAssertFalse(testVC.averageIsAbove(75, 75, 75), "Average must be over 75 to return true.")
+        XCTAssertFalse(testVC.averageIsAbove75(num1: 75, num2: 75, num3: 75), "Average must be over 75 to return true.")
         
-        XCTAssertTrue(testVC.averageIsAbove(75.1, 75, 75), "Avererage is above 75, should return true.")
+        XCTAssertTrue(testVC.averageIsAbove75(num1: 75.1, num2: 75, num3: 75), "Avererage is above 75, should return true.")
         
-        XCTAssertTrue(testVC.averageIsAbove(80, 80, 80), "Average is above 75, should return true.")
+        XCTAssertTrue(testVC.averageIsAbove75(num1: 80, num2: 80, num3: 80), "Average is above 75, should return true.")
         
     }
     
