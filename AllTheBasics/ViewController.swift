60a1ecdfdead827076596c277fc12c8b5b7c8c0d
Justin Choi <jusschoi@gmail.com>
2016-11-07 01:08:00 -0800

    Lab Complete

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -13,9 +13,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print(averageIsAbove75(55, 25, 24))
+        passwordCombo(username: "Michael", pword: 21)
     }
     
     // Implement your functions here!
     
+    func averageIsAbove75(_ a: Double, _ b: Double, _ c: Double) -> Bool {
+        if ((a + b + c) > 75.0) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
+    func passwordCombo(username: String, pword: Int) -> String {
+        if ((username == "Jerry") || (username == "Elaine" || username == "Michael") && ((pword % 3) == 0)) {
+            return "Welcome!"
+        }
+            else {
+                return "Access Denied"
+            }
+        }
+    
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "kiss": return "Kiss"
+        case "cat": return "Cat"
+        case "turtle":return" Turtle"
+        default: return "unknown"
+        }
+    }
 }
+
