736614ecf3677612b415ec1330ed5bfac4264a6b
aviflombaum <avi@flombaum.com>
2016-11-18 19:49:03 -0500

    Done

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -16,6 +16,38 @@ class ViewController: UIViewController {
     }
     
     // Implement your functions here!
-    
-
+   func averageIsAbove(_ a: Double, _ b: Double, _ c: Double) -> Bool {
+    if (a + b + c) / 3 > 75 {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  func passwordCombo(username: String, password: Int) -> String{
+    if ((username == "Jerry" || username == "Elaine" || username == "Michael") && (password % 3 == 0)) {
+      return "Welcome!"
+    } else {
+      return "Access Denied"
+    }
+  }
+  
+  func describe(emoji: String) -> String {
+    switch(emoji){
+    case "💋":
+      return "Kiss"
+    case "🐈":
+      return "Cat"
+    case "🐢":
+      return "Turtle"
+    case "🍕":
+      return "Pizza"
+    case "👻":
+      return "Ghost"
+    case "💩":
+      return "Unknown"
+    default:
+      return "Not Emoji"
+    }
+  }
 }
