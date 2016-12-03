efe569af50ab50bbddec2a4b16f45025f213d504
rlopez178 <rlopez178@yahoo.com>
2016-12-03 16:34:28 -0500

    Finished

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -17,5 +17,50 @@ class ViewController: UIViewController {
     
     // Implement your functions here!
     
+//**1**
+    
+    func averageAbove75(a:Float,b:Float,c:Float)->Bool{
+        let avr = (a+b+c)/3
+        if avr >= 75{
+            return(true)
+        } else {
+            return(false)
+        }
+    }
+    
+    
+    
+
+//**2**
+    
+    func isForce(name:String)->Bool{
+        switch name{
+        case "luke","Leia","Anakin","Obi Wan","Yoda","Vader":
+            return(true)
+        default:
+            return(false)}
+    }
+
+
+
+
+//**3**
+
+func describe(Emoji:String)->String{
+    switch Emoji{
+    case "💋":
+        return("kiss")
+    case "🐈":
+        return("Cat")
+    case "🐢":
+        return("Turtle")
+    case "🍕":
+        return("Pizza")
+    case "👻":
+        return("Ghost")
+    default:
+        return("nun")
+    }
+}
 
 }
