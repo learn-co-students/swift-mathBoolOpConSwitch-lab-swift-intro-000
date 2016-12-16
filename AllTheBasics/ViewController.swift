983111a91a16020042be63f2246b02b4862804d7
jennwent <j@studioteu.com>
2016-12-15 18:22:50 -0800

    Solve problems

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,7 +15,56 @@ class ViewController: UIViewController {
         
     }
     
-    // Implement your functions here!
+    func averageIsAbove75(a:Double, b:Double, c:Double) -> Bool{
+        if (a + b + c) / 3.0 > 75.0{
+            return true}
+        else {
+            return false
+        }
+    }
     
+    func passwordCombo(username:String, password:Int) -> String{
+        if(username == "Jerry" && password % 3 == 0){
+            return("Welcome!")
+        }
+        else if(username == "Elaine" && password % 3 == 0){
+            return("Welcome!")
+        }
+        else if(username == "Michael" && password % 3 == 0){
+            return("Welcome!")
+        }
+        else {
+            return("Access Denied")
+        }
+    }
+    
+    func passwordCombo2(username:String, password:Int) -> String{
+        switch (username == "Jerry" || username == "Elaine" || username == "Michael") && password % 3 == 0{
+        case true:
+            return "Welcome!"
+        default:
+            return "Access Denied"
+        }
+        
+    }
+    
+    func describe(emoji:String) -> String{
+        switch emoji{
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
+    
+    // Implement your functions here!
 
 }
