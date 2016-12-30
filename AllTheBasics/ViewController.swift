01cdc4da92254b2941bfb8499f0957f4682e5e73
4Ring <ukrainianslavic@gmail.com>
2016-12-30 13:31:55 -0800

    Completed Lab

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,7 +15,46 @@ class ViewController: UIViewController {
         
     }
     
-    // Implement your functions here!
+    func averageIsAbove75(_ a: Double, _ b: Double, _ c: Double) -> Bool {
+        
+        if (a + b + c) / 3.0 > 75.0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func passwordCombo(username: String, password: Int) -> String {
+        
+        if username == "Jerry" && password % 3 == 0 {
+            return("Welcome")
+        } else if username == "Elaine" && password % 3 == 0 {
+            return("Welcome")
+        } else if username == "Michael" && password % 3 == 0 {
+            return("Welcome")
+        } else {
+            return("Access Denied")
+        }
+        
+    }
+    
+    func describe(emoji: String) -> String {
+        
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
     
 
+    }
 }
