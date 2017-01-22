b9542ef12ac61b503fec7883fb14f869cbd9a2e0
Terrence <Terrence@Terrences-iMac.local>
2017-01-22 17:29:10 -0500

    ReturnOfTheMack

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -14,10 +14,40 @@ class ViewController: UIViewController {
     
     
     
-    // Implement your functions here
-    
-    
-    
-    
-
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        if (a + b + c) / 3 > 75 {
+        return true
+        } else {
+        return false
+                }
+    }
+    func passwordCombo(username: String, password: Int) -> String {
+        let number = password % 3 == 0
+        switch (username,Bool()) {
+        case ("Jerry",!number):
+            return "Welcome!"
+        case ("Michael",!number):
+            return "Welcome!"
+        case ("Elaine",!number):
+            return "Welcome!"
+        default:
+            return "Access Denied"
+        }
+    }
+    func describe(emoji:String) -> String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
 }
