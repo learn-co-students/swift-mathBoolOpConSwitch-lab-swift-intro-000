573568c7a14a6d14909d581608eef0db6d2c3657
ittakesii <julian@alumni.american.edu>
2017-01-28 14:34:27 -0500

    Completed problems

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -12,12 +12,43 @@ class ViewController: UIViewController {
     
     
     
-    
+
     
     // Implement your functions here
     
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> (Bool) {
+        switch ((a+b+c)/3.0)>75.0 {
+        case true:
+            return true
+        default:
+            return false
+        }
+    }
+
+    func passwordCombo(username: String, password: Int) -> (String) {
+        if (username == "Jerry" || username == "Elaine" || username == "Michael") && (password%3==0) {
+            return "Welcome!"
+        } else {
+            return "Access Denied"
+        }
+    }
     
-    
-    
+    func describe(emoji: String) -> (String) {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
+
 
 }
