eaffb5abfbdc944d4393bd816dfb3e85ac573481
tormic <tormic1992@gmail.com>
2017-07-30 11:38:33 +0200

    done

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -10,14 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    func averageIsAbove75(a: Double, b: Double, c: Double) ->Bool {
+        switch (a + b + c)/3 > 75 {
+        case true:
+            return true
+        default:
+            return false
+        }
+    }
     
-    
-    
-    
-    // Implement your functions here
-    
-    
-    
-    
+    func passwordCombo(username: String, password: Int) ->String {
+    var user = false
+    var pass = false
+        switch username {
+            case "Jerry", "Elaine", "Michael":
+            user = true
+        default:
+            user = false
+        }
+        
+        switch password%3 {
+        case 0:
+            pass = true
+        default:
+            pass = false
+        }
+        switch (user==true && pass==true) {
+        case true:
+            return "Welcome!"
+        default:
+            return "Access Denied"
+        }
 
 }
+    
+    func describe(emoji: String) ->String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
+}
+
+
