9b16e74bac65ff0a8fa29b794b72653caae5f65a
JG <Jennygao1023@gmail.com>
2017-08-17 13:48:54 -0400

    Complete Math Bool Op Conditional Siwtch Lab intro-000

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -10,14 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //#1
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        let average = (Double(a) + Double(b) + Double(c)) / 3.0
+        
+        if average > 75.0 {
+            return true
+        } else {
+            return false
+        }
+    }
     
     
+    //#2
+    func passwordCombo(username: String, password: Int) -> String {
+        if Int(password) % 3 == 0 {
+            switch username {
+                case "Jerry", "Elaine", "Michael":
+                    return "Welcome!"
+        
+                default:
+                    return "Access Denied"
+            }
+        }
+        return "Access Denied"
+    }
     
-    
-    // Implement your functions here
-    
-    
-    
-    
+    //#3
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
+
 
 }
