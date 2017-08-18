2f1d4cf19930af2ba8dd0bf2784ea17f1e6fbfdf
Michael Jackson <mj94@mj94.net>
2017-08-18 18:11:11 -0500

    Completed Math Bool Op Con Switch Lab

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -9,15 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        let average = (a + b + c) / 3
+        
+        if average > 75 {
+            return true
+        } else {
+            return false
+        }
     
+    }
     
+    func passwordCombo(username: String, password: Int) -> String {
+        var usernameCorrect: Bool
+        
+        switch username {
+        case "Jerry", "Elaine", "Michael":
+            usernameCorrect = true
+        default:
+            usernameCorrect = false
+        }
+        
+        if usernameCorrect && password % 3 == 0 {
+            return "Welcome!"
+        } else {
+            return "Access Denied"
+        }
+    }
     
-    
-    
-    // Implement your functions here
-    
-    
-    
-    
-
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
 }
