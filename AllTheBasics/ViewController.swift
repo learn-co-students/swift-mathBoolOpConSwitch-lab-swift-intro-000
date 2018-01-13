88261602455dd350650d20187b74994e5d249282
Shreyak Godala <shreyakgodala2909@gmail.com>
2018-01-13 22:59:35 +0530

    Add mathBoolOp swift lab

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    func averageIsAbove75(a: Double, b:Double, c:Double) -> Bool{
+        let average = (a+b+c)/3.0
+        if average > 75.0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func passwordCombo(username: String, password: Int) -> String {
+        
+        if username == "Jerry" || username == "Elaine" || username == "Michael"{
+            if password % 3 == 0 {
+                return "Welcome!"
+            } else {
+                return "Access Denied"
+            }
+        } else {
+            return "Access Denied"
+        }
+        
+    }
     
     
+    func describe(emoji: String) -> String {
+        switch emoji{
+        case "💋" :
+            return "Kiss"
+        case "🐈" :
+            return "Cat"
+        case "🐢" :
+            return "Turtle"
+        case "🍕" :
+            return "Pizza"
+        case "👻" :
+            return "Ghost"
+        default :
+            return "Unknown"
+        }
+    }
     
-    
-    // Implement your functions here
     
     
     
