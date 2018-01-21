7b87fbea0d9efb5035bc0d9b13c18d28d1260ff7
tsm121 <tsm121@gmail.com>
2018-01-21 23:37:49 +0100

    Commit

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -14,10 +14,44 @@ class ViewController: UIViewController {
     
     
     
-    // Implement your functions here
+    func averageIsAbove75(a:Double,b:Double,c:Double) -> Bool {
+        let avr = (a+b+c)/3
+        if avr > 75 {
+            return true
+        }
+        return false
+    }
     
-    
-    
+    func passwordCombo(username:String,password:Int) -> String {
+
+        if password%3 != 0 {
+            return "Access Denied"
+        }
+        
+        switch username {
+        case "Jerry", "Elaine", "Michael":
+            return "Welcome!"
+        default:
+            return "Access Denied"
+        }
+    }
+
+    func describe(emoji:String) -> String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
     
 
 }
