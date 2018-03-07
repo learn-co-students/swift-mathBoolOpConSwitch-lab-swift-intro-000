268eb91bde3c1e211b791bff6c63d100cebceaed
Stanley Sakai <sakai.stanley@gmail.com>
2018-03-07 14:23:10 -0500

    Math Bool Op Con Switch Lab

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -10,14 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        switch (a + b + c) / 3 > 75 {
+            case true:
+                return true
+            default:
+                return false
+        }
+    }
     
+    func passwordCombo(username: String, password: Int) -> String {
+        var result = "Access Denied"
+        
+        switch username {
+            case "Jerry", "Elaine", "Michael":
+                if password % 3 == 0 {
+                    result = "Welcome!"
+                }
+            default:
+                result = "Access Denied"
+        }
+        
+        return result
+    }
     
-    
-    
-    // Implement your functions here
-    
-    
-    
-    
-
+    func describe(emoji: String) -> String {
+        switch emoji {
+            case "💋":
+                return "Kiss"
+        
+            case "🐈":
+                return "Cat"
+            
+            case "🐢":
+                return "Turtle"
+        
+            case "🍕":
+                return "Pizza"
+            
+            case "👻":
+                return "Ghost"
+        
+            default:
+                return "Unknown"
+        }
+    }
 }
