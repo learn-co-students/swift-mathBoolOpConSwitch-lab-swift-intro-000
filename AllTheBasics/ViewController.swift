70db1dda44a286adf76fb5f2c9c1fbf78801fc5a
Vinod <vinod.wst@gmail.com>
2018-03-07 12:19:02 +0530

    changed
    changed

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -12,8 +12,71 @@ class ViewController: UIViewController {
     
     
     
+    func averageIsAbove75(a:Double, b:Double, c:Double) ->Bool{
+        var result:Bool
+        let  d:Double = (a + b + c ) / 3
+       /* switch d {
+        case Int.max ... 75:
+        result =  true
+         default:
+         result =  false
+        }
+         
+         
+ */
+       
+        switch d{
+       case let x where x > 75.0:
+      result =  true
+        default:
+            result =  false
+        }
+        return  result
+    }
     
+  
+    func passwordCombo(username:String, password:Int) ->  String{
+        
+        var strCorrect:Bool =  false;
+         switch username{
+         case "Jerry":
+            strCorrect =  true
+         case "Elaine":
+         strCorrect =  true
+         case "Michael":
+         strCorrect =  true
+         default:
+            strCorrect = false
+        }
+        
+        if(strCorrect == true   && password % 3 == 0 ){
+            return "Welcome!"
+        }else{
+            return "sorry"
+        }
+        // // 
+    }
     
+
+    func describe(emoji:String) ->String {
+        
+        switch emoji{
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+            
+        
+    }
     // Implement your functions here
     
     
