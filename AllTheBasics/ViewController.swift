61c59ba8fbc26dc321005c3d3790d834c576ccdd
cdelcar0 <dangdude14@yahoo.com>
2018-09-15 12:30:01 -0400

    emoji's for days

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    func averageIsAbove75(a: Double,b: Double, c: Double) -> Bool{
+        var result = (a+b+c)/3
+        switch result{
+        case 0...75 :
+            return false
+        default :
+            return true
+        }
+    }
     
+    func passwordCombo(username: String, password: Int) -> String{
+        if(password % 3 != 0){
+            return "Access Denied"
+        } else {
+            switch username{
+            case "Jerry", "Elaine", "Michael" :
+                return "Welcome!"
+            default :
+                return "Access Denied"
+            }
+        }
+    }
     
-    
-    
-    
-    // Implement your functions here
-    
-    
-    
-    
-
+    func describe(emoji: String) -> String{
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default :
+            return "Unknown"
+        }
+    }
 }
