96de57e4b04f4776fb565ce663f8b2bdb03a105e
Kegan <kronholt273@gmail.com>
2018-10-20 15:45:48 +0800

    swift-mathBoolOpConSwitch-lab-swift-intro-000

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  ViewController.swift
 //  AllTheBasics
 //
@@ -16,8 +16,59 @@ class ViewController: UIViewController {
     
     // Implement your functions here
     
+    func averageIsAbove75(a: Double, b: Double,  c: Double) -> Bool{
+        let ave = a + b + c
+        let average = ave / 3.0
+        if average > 75.0 {
+            return true
+        }
+        else{
+            return false
+        }
+    }
     
+    func passwordCombo(username: String, password: Int) -> String{
+        
+        
+        
+        let isTrue = password % 3 == 0
+        if isTrue{
+            
+        
+        switch username {
+        case "Jerry":
+            return "Welcome!"
+        case "Elaine":
+            return "Welcome!"
+        case "Michael":
+            return "Welcome!"
+        default:
+            return "Access Denied"
+        }
+        }
+        else {
+                return "Access Denied"
+            }
+
     
+    }
     
+    func describe(emoji: String) -> String{
+        
+        switch emoji{
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
 
 }
